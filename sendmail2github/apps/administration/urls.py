6f7bc53412f65d20ff7d23c966641caaf74ba421
shequet <shequet@gmail.com>
2021-06-08 11:23:38 +0200

    add admin users

--- sendmail2github/apps/administration/urls.py
+++ sendmail2github/apps/administration/urls.py
@@ -2,7 +2,7 @@
 Urls for the website app
 """
 from django.urls import path
-from .views import user_add, users, user_show
+from .views import user_add, users, user_show, user_edit, user_delete
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,6 +11,8 @@ urlpatterns = [
     path('', users, name='users'),
     path('<int:user_id>', user_show, name='user_show'),
     path('add/', user_add, name='user_add'),
+    path('edit/<int:user_id>', user_edit, name='user_edit'),
+    path('delete/<int:user_id>', user_delete, name='user_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- sendmail2github/apps/administration/views.py
+++ sendmail2github/apps/administration/views.py
@@ -2,7 +2,8 @@
 website app views document
 """
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -11,13 +12,20 @@ from .forms import CustomUserCreationForm
 
 @login_required
 def user_show(request, user_id):
+    try:
+        return render(
+            request,
+            'user_show.html', {
+                'user': User.objects.get(id=user_id)
+            })
+    except User.DoesNotExist:
 
-    return render(
-        request,
-        'user_show.html', {
-            'user': User.objects.get(id=user_id)
-        })
-
+        return render(
+            request,
+            'users.html', {
+                'users': User.objects.values(),
+                'message': "L'utilisateur n'existe pas"
+            })
 
 @login_required
 def users(request):
@@ -40,7 +48,7 @@ def user_add(request):
 
     if request.method == "GET":
         return render(
-            request, "registration/register.html",
+            request, "user_add.html",
             {
                 "form": CustomUserCreationForm
             }
@@ -50,4 +58,64 @@ def user_add(request):
         if form.is_valid():
             user = form.save()
             return redirect('user_show', user_id=user.id)
+        else:
+            return render(
+                request, "user_add.html",
+                {
+                    "form": CustomUserCreationForm,
+                    "errors": form.error_messages
+                }
+            )
 
+
+@login_required
+def user_edit(request, user_id):
+    """
+    Display register user of the site
+    """
+
+    if request.method == "GET":
+        return render(
+            request, "user_edit.html",
+            {
+                "form": CustomUserCreationForm(instance=User.objects.get(id=user_id))
+            }
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST, instance=User.objects.get(id=user_id))
+
+        if form.is_valid():
+            update = form.save(commit=False)
+            update.user = request.user
+            update.save()
+
+            return redirect('user_show', user_id=update.id)
+        else:
+            return render(
+                request, "user_edit.html",
+                {
+                    "form": CustomUserCreationForm(instance=User.objects.get(id=user_id)),
+                    "errors": form.error_messages
+                }
+            )
+
+
+@permission_required('is_superuser')
+def user_delete(request, user_id):
+    all_users = User.objects.values()
+    try:
+        user = User.objects.get(id=user_id)
+        user.delete()
+        return render(
+            request,
+            'users.html', {
+                'users': all_users,
+                'message': "L'utilisateur est supprimé"
+            })
+    except User.DoesNotExist:
+        return render(
+            request,
+            'users.html', {
+                'users': all_users,
+                'message': "L'utilisateur n'existe pas"
+            })
